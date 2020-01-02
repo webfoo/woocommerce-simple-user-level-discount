b83dcb12a3536ece11a4f3b825c588f11848121d
Kevin Ruscoe <hello@kevinruscoe.me>
2020-01-02 19:45:39 +0000

    Clean up some stuff

--- templates/user_level_discount_tab.php
+++ templates/user_level_discount_tab.php
@@ -1,5 +1,9 @@
 <h2>User Level Discount</h2>
 
+<p>
+    Use the table below to add your discounts.
+</p>
+
 <div id="app">
     <user-level-table 
         name="<?php print Woocommerce_Simple_User_Level_Discount_Field::get_field_name() ?>"
